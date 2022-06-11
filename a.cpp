c23cd4e1fd6fcbb000715d98e52d521e71831461
Aman Kumar <amankumarmznup@gmail.com>
2022-06-12 04:46:04 +0530

    last

--- a.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-for(int numero =4;numero>=0;numero--){
-    cout<<numero<<" libros en la repisa.\n";
-    if(numero >0) cout<<"Baja un libro y pasalo a tu companero. \n";
-    else cout<<"No hay mas libros. \n";
-}
-}
